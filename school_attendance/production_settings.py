b64b9217849b5ea74f8e194a3732b38469b33848
abhis40 <abhishek.12109384@gmail.com>
2025-04-23 22:13:27 +0530

    Update production database settings for PythonAnywhere

--- school_attendance/production_settings.py
+++ school_attendance/production_settings.py
@@ -18,6 +18,10 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
