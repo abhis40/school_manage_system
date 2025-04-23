d75a5a965b26c26a2c6db60cae1320cc221d706a
abhis40 <abhishek.12109384@gmail.com>
2025-04-23 22:20:22 +0530

    Add environment variables to WSGI configuration

--- school_attendance/pythonanywhere_wsgi.py
+++ school_attendance/pythonanywhere_wsgi.py
@@ -10,6 +10,13 @@ if path not in sys.path:
 os.environ['DJANGO_SETTINGS_MODULE'] = 'school_attendance.production_settings'
 os.environ['PYTHONPATH'] = path
 
+# Production environment variables
+os.environ['DJANGO_SECRET_KEY'] = '5h%uxgn6&+f+3mbs9_k6&jysg$^n16s9@op=%dk6@n81_$^d-%'
+os.environ['DB_NAME'] = '<your-pythonanywhere-username>$school_db'
+os.environ['DB_USER'] = '<your-pythonanywhere-username>'
+os.environ['DB_PASSWORD'] = '<your-mysql-password>'
+os.environ['DB_HOST'] = '<your-pythonanywhere-username>.mysql.pythonanywhere-services.com'
+
 # Import your Django project
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
